325f12cec3cfaea487ad62d02a5923518179d7cd
volodia.g <volodia.gp@gmail.com>
2018-09-16 13:25:57 +0300

    markdown example

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,23 +1,67 @@
 import React, { Component } from 'react';
-import './App.css';
+import { markdown } from 'markdown';
+import './App.scss';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      inputValue: '',
+      inputTransformed: '',
+      selectedMarkdown: 'markdown'
     }
   }
 
+  returnTransformedInput(inputValue) {
+    const { selectedMarkdown } = this.state;
+
+    let inputTransformed = '';
+    switch (selectedMarkdown) {
+      case 'markdown':
+        inputTransformed = markdown.toHTML(inputValue);
+        break;
+      default:
+        inputTransformed = markdown.toHTML(inputValue);
+    }
+    return inputTransformed;
+  }
+
+  handleInput(event) {
+    this.setState({ inputValue: event.target.value });
+  }
+
   render() {
+    const { inputValue } = this.state;
+    const transformedInput = this.returnTransformedInput(inputValue);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app-container">
+        <div className="markdown-container">
+          <div className="transform-container">
+            <div className="title">Input text</div>
+            <div className="text-container input-container">
+              <textarea
+                onChange={(event) => this.handleInput(event)}
+                value={inputValue}
+                rows="4"
+                cols="50"
+                placeholder="Input text here"
+              />
+            </div>
+          </div>
+          <div className="transform-container">
+            <div className="title">HTML</div>
+            <div className="text-container html-container">
+              {transformedInput}
+            </div>
+          </div>
+          <div className="transform-container">
+            <div className="title">Result</div>
+            <div className="text-container result-container">
+              <div dangerouslySetInnerHTML={{ __html: transformedInput }} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
